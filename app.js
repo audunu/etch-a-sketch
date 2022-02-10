482996e3149043dcb4b1436a77fc28e64896c4c7
audunu <audunu@gmail.com>
2022-02-10 13:20:20 +0100

    add a restart button and option to choose number of pixels

--- app.js
+++ app.js
@@ -3,20 +3,48 @@
 const container = document.querySelector('.grid-container');
 const btn = document.querySelector('.button');
 
-for (let i = 1; i <= (16*16); i++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-item', `grid-item-${i}`);
-    square.innerText = `${i}`;
-    
-    square.addEventListener('mouseover', e => {
-        e.target.classList.add('color')});
-    
-    container.appendChild(square);
+function createGrid(numberOfRows) {
+    container.style.cssText = `grid-template-columns: repeat(${numberOfRows}, 1fr); grid-template-rows: repeat(${numberOfRows}  , 1fr)`;
+    for (let i = 1; i <= (numberOfRows*numberOfRows); i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-item', `grid-item-${i}`);
+        
+        
+        square.addEventListener('mouseover', e => {
+            e.target.classList.add('color')});
+        
+        container.appendChild(square);
+    }    
 }
 
-btn.addEventListener('click', newGrid);
 
-function newGrid() {
+function clearGrid() {
+    /* const allSquares = document.querySelectorAll('.grid-item');
+    for (const square of allSquares) {
+        square.classList.remove('color');
+    } */
+
+    container.innerHTML = '';
     
-    prompt('enter desired number of rows/columns for a new grid');
+    
 }
+
+function getUserInput() {
+    let userInput = parseInt(prompt('enter desired number of rows/columns for a new grid. min 1 max 100.', 16));
+    while(isNaN(userInput) || userInput > 100 || userInput < 1) {
+        userInput = parseInt(prompt('enter desired number of rows/columns for a new grid. min 1 max 100.', 16));
+    }
+    return userInput;
+
+}
+
+
+
+btn.addEventListener('click', () => {;
+    clearGrid();
+    createGrid(getUserInput());
+});
+
+
+
+createGrid(16);
